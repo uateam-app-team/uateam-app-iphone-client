0f18f7c78ec8d8f883f3f1c361536cf016193bcf
Andrii Titov <concuror@gmail.com>
2012-07-01 15:46:06 +0300

    Added description entity

---
+++ UATeamClient/Classes/Descriptions/UATReleaseDescription.h
@@ -0,0 +1,29 @@
+//
+//  UATReleaseDescription.h
+//  UATeamClient
+//
+//  Created by Andrii Titov on 7/1/12.
+//  Copyright (c) 2012 uateam-app. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UATReleaseDescription : NSObject
+
+@property (nonatomic, strong) NSString *description;
+
+@property (nonatomic, strong) NSString *category;
+
+@property (nonatomic, strong) NSString *name;
+
+@property (nonatomic, strong) NSString *country;
+
+@property (nonatomic, strong) NSString *genre;
+
+@property (nonatomic, strong) NSString *director;
+
+@property (nonatomic, strong) NSString *cast;
+
+@property (nonatomic, strong) NSString *imageSrc;
+
+@end
---
+++ UATeamClient/Classes/Descriptions/UATReleaseDescription.m
@@ -0,0 +1,13 @@
+//
+//  UATReleaseDescription.m
+//  UATeamClient
+//
+//  Created by Andrii Titov on 7/1/12.
+//  Copyright (c) 2012 uateam-app. All rights reserved.
+//
+
+#import "UATReleaseDescription.h"
+
+@implementation UATReleaseDescription
+
+@end
